4b7865e88aca05bb9bf2dd7657799d7d647c90ea
shulandt <shulandt.aleksandr@gmail.com>
2022-07-26 14:39:27 +0100

    update

--- sdr_scan.sh
+++ sdr_scan.sh
@@ -7,10 +7,11 @@ then
     echo Usage sdr_scan.sh min_freq max_freq output_file
     exit
 fi
-echo scanning from $1 MHz to $2 MHz ...
-while [ 1 == 1 ]
-    do
-        sudo rtl_power -g 0 -i 3 -1 -w hamming -f $1M:$2M:100 rtl-power.csv > /dev/null 2>&1
+step=`echo "($2 - $1) * 50" | bc`
+echo scanning from $1 MHz to $2 MHz, step $step Hz ...
+#while [ 1 == 1 ]
+#    do
+        sudo rtl_power -g 0 -i 3 -1 -w hamming -f $1M:$2M:$step rtl-power.csv > /dev/null 2>&1
         rtl-scan rtl-power.csv $3
-    done
-#python3 flatten.py rtl_power.csv > rtl_power_sort.csv
+#    done
+#python3 flatten.py rtl-power.csv > rtl-power-sort.csv
