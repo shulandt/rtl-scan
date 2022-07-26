385d1b5b29e98fc05edd68868dfb835b54261851
shulandt <shulandt.aleksandr@gmail.com>
2022-07-26 10:55:47 +0100

    script update

--- sdr_scan.sh
+++ sdr_scan.sh
@@ -2,10 +2,15 @@
 
 trap "echo stop; exit;" SIGINT SIGTERM
 
-echo scanning...
+if [ $# -ne 3 ]
+then
+    echo Usage sdr_scan.sh min_freq max_freq output_file
+    exit
+fi
+echo scanning from $1 MHz to $2 MHz ...
 while [ 1 == 1 ]
     do
-        sudo rtl_power -g 0 -i 3 -1 -w hamming -f 461M:464M:100 rtl-power.csv > /dev/null 2>&1
-        rtl-scan rtl-power.csv
+        sudo rtl_power -g 0 -i 3 -1 -w hamming -f $1M:$2M:100 rtl-power.csv > /dev/null 2>&1
+        rtl-scan rtl-power.csv $3
     done
 #python3 flatten.py rtl_power.csv > rtl_power_sort.csv
