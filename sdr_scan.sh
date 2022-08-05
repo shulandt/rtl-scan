cb1d546047eedbff2728cbeee7670c5f667369a0
Oleksandr <shulandt.aleksandr@gmail.com>
2022-08-05 14:11:34 +0300

    update

--- sdr_scan.sh
+++ sdr_scan.sh
@@ -2,17 +2,27 @@
 
 trap "echo stop; exit;" SIGINT SIGTERM
 
-if [ $# -ne 7 ]
-then
-    echo Usage sdr_scan.sh device min_freq max_freq freq_interval min_bandwidth min_snr output_file
-    exit
-fi
-step=`echo "($3 - $2) * 50" | bc`
-echo scanning from $2 MHz to $3 MHz, step $step Hz, freq interval $4 MHz, min bandwidth $5 MHz, min snr $6 dB 
+#SDR device number
+DEVICE=0
+#Begin frequency, MHz
+BEGIN_FREQ=462.0
+#End frequency, MHz
+END_FREQ=463.8
+#Min frequency interval between signals, kHz
+INTERVAL_FREQ=100.0
+#Min bandwidth for signal, kHz
+MIN_BANDWIDTH=1.0
+#Min SNR for signal, dB
+MIN_SNR=10
+#Output file name
+OUT_FILE=freq_scan.csv
+
+STEP=`echo "($END_FREQ - $BEGIN_FREQ) * 50" | bc`
+echo scanning from $BEGIN_FREQ MHz to $END_FREQ MHz, step $STEP Hz, freq interval $INTERVAL_FREQ kHz, min bandwidth $MIN_BANDWIDTH kHz, min snr $MIN_SNR dB 
 while [ 1 == 1 ]
     do
         echo -n .
-        sudo rtl_power -d $1 -g 20 -i 3 -1 -w hamming -f $2M:$3M:$step rtl-power.csv > /dev/null 2>&1
-        rtl-scan rtl-power.csv $7 $4 $5 $6
+        sudo rtl_power -d $DEVICE -g 20 -i 3 -1 -w hamming -f $BEGIN_FREQM:$END_FREQM:$step rtl-power.csv > /dev/null 2>&1
+        rtl-scan rtl-power.csv $OUT_FILE $INTERVAL_FREQ $MIN_BANDWIDTH $MIN_SNR
     done
 #python3 flatten.py rtl-power.csv > rtl-power-sort.csv
